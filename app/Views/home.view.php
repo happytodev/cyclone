96ad5aa4fb6d4fa0eedd32d04b3eb99a4d52f96e
Frédéric Blanc <frederic.blanc@itanea.fr>
2025-06-21 19:09:59 +0200

    fix: bug on collision between last news and post and fix typo

--- app/Views/home.view.php
+++ app/Views/home.view.php
@@ -19,7 +19,7 @@
                     </div>
                     <!-- Text -->
                     <h1 class="text-balance text-5xl font-semibold tracking-tight text-gray-800 sm:text-7xl">Cyclone</h1>
-                    <p class="mt-8 text-pretty text-lg font-medium text-gray-500 sm:text-xl/8">The first blog engine based on Tempest.</p>
+                    <p class="mt-8 text-pretty text-lg font-medium text-gray-500 sm:text-xl/8">The firstly blog engine based on Tempest.</p>
                     <!-- CTAs -->
                     <div class="mt-10 flex flex-col sm:flex-row gap-y-4 items-center justify-center gap-x-6">
                         <a href="#" target="_blank" class="rounded-md bg-sky-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-sky-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600">Documentation</a>
@@ -94,7 +94,7 @@
                         <h2 class="text-3xl font-bold text-gray-800 mb-8 text-center">Latest news</h2>
                         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
                             <!-- Last 3 posts -->
-                            <x-post :foreach="$posts as $post" >
+                            <x-post :foreach="$posts as $lastnew" >
                                 <x-card />
                             </x-post>
                             <x-post :forelse>
