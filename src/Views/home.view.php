7e93046b6adf42e2851ca7b99e0efb7a13c08413
Frédéric Blanc <frederic.blanc@itanea.fr>
2025-07-11 01:00:44 +0200

    chore: talk about open source and the great milkdown

--- src/Views/home.view.php
+++ src/Views/home.view.php
@@ -66,10 +66,21 @@
                                     </svg> -->
                                 </div>
                                 <h3 class="text-lg font-semibold text-gray-800 mb-2">Open source</h3>
-                                <p class="text-gray-600">Create your content directly with markdown syntax. Happy to geek ;-)</p>
+                                <p class="text-gray-600">Cyclone is open source, you can contribute to it. Open source is the future.</p>
                             </div>
                             
                             
+                            <!-- Service 3 -->
+                            <div class="bg-white rounded-lg shadow-md p-6 hover:shadow-lg transition-shadow duration-300">
+                                <div class="w-12 h-12 bg-blue-100 rounded-full flex items-center justify-center mb-4">
+                                    <x-icon name="tdesign:milk" class="size-8 text-indigo-400" />
+                                    <!-- <svg class="w-6 h-6 text-blue-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z"></path>
+                                    </svg> -->
+                                </div>
+                                <h3 class="text-lg font-semibold text-gray-800 mb-2">Using Milkdown editor</h3>
+                                <p class="text-gray-600">Create your content visually with the power of Milkdown and markdown.</p>
+                            </div>
                             <!-- Service 3 -->
                             <div class="bg-white rounded-lg shadow-md p-6 hover:shadow-lg transition-shadow duration-300">
                                 <div class="w-12 h-12 bg-blue-100 rounded-full flex items-center justify-center mb-4">
